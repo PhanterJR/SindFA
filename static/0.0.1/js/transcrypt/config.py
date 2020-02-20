b2921bafd195eb5c74d17e6f8f7baa525cac7686
PhanterJR <junior.conex@gmail.com>
2020-02-20 19:07:47 -0300

    correction bug flash

--- static/0.0.1/js/transcrypt/config.py
+++ static/0.0.1/js/transcrypt/config.py
@@ -11,7 +11,7 @@ CONFIG = {
         "name": "SindFA",
         "title": "PhanterEDU",
         "version": "0.0.1",
-        "compilation": 2824,
+        "compilation": 2825,
         "author": "PhanterJR<phanterjr@conexaodidata.com.br>",
         "debug": True,
         "packaged": False,
