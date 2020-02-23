3ff0cfaf63d6efed6f25afe3dc6023b3b37afbc7
PhanterJR <junior.conex@gmail.com>
2020-02-23 16:26:07 -0300

    phanterpwa 4.1.6

--- static/0.0.1/js/transcrypt/config.py
+++ static/0.0.1/js/transcrypt/config.py
@@ -11,7 +11,7 @@ CONFIG = {
         "name": "SindFA",
         "title": "PhanterEDU",
         "version": "0.0.1",
-        "compilation": 2862,
+        "compilation": 2865,
         "author": "PhanterJR<phanterjr@conexaodidata.com.br>",
         "debug": True,
         "packaged": False,
